0ea6866c87c0575428c7ca794ca0c220a1e9f0a2
Sanusi Hassan <sanusihassan122@gmail.com>
2023-10-07 19:14:56 +0000

    working split-by-range version but without original file names

--- routes/split_pdf.py
+++ routes/split_pdf.py
@@ -1,14 +1,19 @@
+# sudo apt-get install poppler-utils
+# here is the updated code, it's still not working as expected
+# gitpod /workspace/pdfequips-api (master) $ ls /tmp
+# files-0.pdf  files-1.pdf  files-2.pdf
+"""
+   i want a function called split_by_range 
+"""
+
+# this is how i'm using the function:
+
 import os
 from tempfile import NamedTemporaryFile
 from flask import after_this_request, jsonify, request, send_file
 from tools.split_pdf import split_by_range
 from utils.utils import validate_file
-"""
-    the ranges is a json object and this is how i'm reciving it and passing it to the split_by_range function
-"""
-
 import json
-
 
 def split_pdf_route(app):
     @app.route('/api/split-pdf', methods=['POST'])
@@ -16,28 +21,51 @@ def split_pdf_route(app):
         if 'files' not in request.files:
             return jsonify({"error": "No PDF file provided"}), 400
         files = request.files.getlist('files')
-        ranges = json.loads(request.form.get('ranges'))  # Convert ranges to JSON object
+        # Convert ranges to JSON object
+        ranges = json.loads(request.form.get('ranges'))
         error = validate_file(files)
         if error:
             response = jsonify(error)
             response.headers['Content-Type'] = 'application/json'
             return jsonify({"error": response}), 400
         if len(files) == 1:
+            # file = files[0]
             file = files[0]
-            split_file = split_by_range(file, ranges)
-            with NamedTemporaryFile(mode='wb', delete=False) as tmp_file:
-                tmp_file.write(bytes('\n'.join(split_file), 'utf-8'))
-                tmp_file.flush()
-                os.fsync(tmp_file.fileno())
-            response = send_file(tmp_file.name, mimetype='application/pdf',
-                                 as_attachment=True, download_name='split.pdf', conditional=True)
+            split_file_path = split_by_range(file, ranges)
+            if not split_file_path:
+                return jsonify({"error": "Error splitting PDF"}), 500
+            if split_file_path.endswith('.pdf'):
+                mimetype = 'application/pdf'
+                download_name = 'split.pdf'
+            else:  # It's a zip file
+                mimetype = 'application/zip'
+                download_name = 'split.zip'
+            response = send_file(split_file_path, mimetype=mimetype,
+                                 as_attachment=True, download_name=download_name, conditional=True)
             response.headers['X-Accel-Buffering'] = 'no'
             response.headers['Cache-Control'] = 'no-cache'
             response.headers['Connection'] = 'close'
 
             @after_this_request
             def remove_file(response):
-                os.remove(tmp_file.name)
+                os.remove(split_file_path)
                 return response
 
             return response
+        else:
+            zip_file_path = split_by_range(files, ranges)
+            if not zip_file_path:
+                return jsonify({"error": "Error splitting PDF"}), 500
+            response = send_file(zip_file_path, mimetype='application/zip',
+                                 as_attachment=True, download_name='split.zip', conditional=True)
+            response.headers['X-Accel-Buffering'] = 'no'
+            response.headers['Cache-Control'] = 'no-cache'
+            response.headers['Connection'] = 'close'
+
+            @after_this_request
+            def remove_file(response):
+                os.remove(zip_file_path)
+                return response
+
+            return response
+
--- tools/split_pdf.py
+++ tools/split_pdf.py
@@ -1,34 +1,67 @@
-import subprocess
-import tempfile
+from PyPDF2 import PdfReader, PdfWriter
 import os
+from tempfile import NamedTemporaryFile
+import zipfile
 
-import subprocess
-import tempfile
-import os
 
-def split_by_range(input_file, ranges):
-    try:
-        # Save the input file to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False) as temp_input:
-            temp_input.write(input_file.read())
-            temp_input_path = temp_input.name
+"""
+    please help me with this function,
+    the tmp files should be named with the exact original filename-{i} example: file: test.pdf ranges: [{from: 1: to: 1}, {from: 2, to: 2}]
+    output files should be test-1.pdf and test-2.pdf and so on you get the idea.
+    also after returning the files the tmp files should be removed.
+    as you can see from the output below they still exist after the return
+    gitpod /workspace/pdfequips-api (master) $ ls /tmp
+    tmp5bfwmsp5  tmp96_b12i_.pdf  tmpktlcmthq.pdf  tmpvezqx9q3.pdf
+    gitpod /workspace/pdfequips-api (master) $ 
+"""
 
-        # Generate the output file names based on the range
-        output_files = []
-        for i, range_obj in enumerate(ranges):
-            output_file = f"output-{i}.pdf"
-            output_files.append(output_file)
+def split_by_range(file, ranges):
+    # Save the file to a temporary folder
+    temp_file = NamedTemporaryFile(delete=False)
+    file.save(temp_file.name)
 
-            # Run the pdfseparate command for each range
-            subprocess.run(['pdfseparate', '-f', str(range_obj['from']), '-l',
-                           str(range_obj['to']), temp_input_path, output_file], check=True)
+    # Open the PDF file
+    pdf = PdfReader(temp_file.name)
 
-        print("PDF split successfully.")
+    # Create a list to store the output file paths
+    output_files = []
+
+    # Iterate over the ranges
+    for range_obj in ranges:
+        start_page = range_obj['from']
+        end_page = range_obj['to']
+
+        # Create a new PDF writer
+        output = PdfWriter()
+
+        # Add the pages within the range to the new PDF writer
+        for page_num in range(start_page - 1, end_page):
+            # PyPDF2.errors.DeprecationError: reader.getPage(pageNumber) is deprecated and was removed in PyPDF2 3.0.0. Use reader.pages[page_number] instead.
+            output.add_page(pdf.pages[page_num])
+            # reader.pages[page_number]
+
+        # Save the output PDF to a new file
+        output_file = NamedTemporaryFile(suffix='.pdf', delete=False)
+        output.write(output_file)
+        output_files.append(output_file.name)
+
+    # If there is only one range, return the file path
+    if len(ranges) == 1:
         return output_files
-    except subprocess.CalledProcessError as e:
-        print(f"Error splitting PDF: {e}")
-        return []
-    finally:
-        # Remove the temporary input file
-        if temp_input_path:
-            os.remove(temp_input_path)
+
+    # If there are multiple ranges, create a zip folder
+    zip_file_path = NamedTemporaryFile(suffix='.zip', delete=False).name
+    with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
+        for file_path in output_files:
+            zip_file.write(file_path, os.path.basename(file_path))
+
+    return zip_file_path
+
+
+def save_file(file):
+    # Create a temporary file to save the uploaded file
+    temp_file = NamedTemporaryFile(delete=False)
+    file.save(temp_file.name)
+
+    # Return the path of the saved file
+    return temp_file.name
