3063872efab5f5809a22ef5d5c55b76e466eec0e
planetcreative61 creativity <planetcreative61@gmail.com>
2023-11-10 00:28:41 +0000

    added md_to_pdf

--- routes/md_to_pdf.py
+++ routes/md_to_pdf.py
@@ -1,9 +1,10 @@
 import os
 import shutil
 from flask import jsonify, request, send_file, after_this_request
-from tools.markdown_to_pdf import md_text_to_pdf
+from tools.markdown_to_pdf import md_text_to_pdf, md_to_pdf
 from utils.utils import validate_file
 import json
+
 
 def md_text_to_pdf_route(app):
     @app.route("/api/md-text-to-pdf", methods=['POST'])
@@ -21,13 +22,32 @@ def md_text_to_pdf_route(app):
 
         return send_file(pdf_path, as_attachment=True, download_name="output.pdf")
 
+
+
+"""
+    now plesse complete the implementation of the below function to use the md_to_pdf you just created
+    and use the @after_this_request to remove the tmp files
+    if the tmp_files ends with .md then just delete the md file, otherwise remove the directory or somthing
+"""
+
 def md_to_pdf_route(app):
-    @app.route("/api/md-to-pdf", methods=['POST'])
+    @app.route("/api/markdown-to-pdf", methods=['POST'])
     def md_to_pdf_handler():
         if 'files' not in request.files:
             return jsonify({"error": "No PDF file provided"}), 400
         files = request.files.getlist('files')
-        if len(files) == 1:
-            pdf_path = md_to_pdf(files[0])
-            pass
-        pass
+        
+        pdf_path, tmp_path = md_to_pdf(files)
+        
+        @after_this_request
+        def remove_tmp_files(response):
+            if os.path.isfile(tmp_path) and tmp_path.endswith('.md'):
+                os.remove(tmp_path)
+            else:
+                shutil.rmtree(tmp_path)
+            return response
+        
+        if tmp_path.endswith('.md'):
+            return send_file(pdf_path, as_attachment=True)
+        else:
+            return send_file(pdf_path, as_attachment=True, attachment_filename='output.zip')
--- tools/markdown_to_pdf.py
+++ tools/markdown_to_pdf.py
@@ -1,5 +1,8 @@
 import tempfile
 import subprocess
+import os
+import shutil
+import zipfile
 
 def md_text_to_pdf(md_text):
     with tempfile.NamedTemporaryFile('w', delete=False, suffix='.md') as temp:
@@ -12,3 +15,26 @@ def md_text_to_pdf(md_text):
     subprocess.run(['mdpdf', temp_path, '-o', pdf_path], check=True)
 
     return temp_path, pdf_path
+
+
+
+def md_to_pdf(files):
+    if len(files) == 1:
+        file = files[0]
+        tmp_folder = tempfile.mkdtemp()
+        tmp_file = os.path.join(tmp_folder, file.filename)
+        file.save(tmp_file)
+        output_file = os.path.join(tmp_folder, 'output.pdf')
+        subprocess.run(['mdpdf', tmp_file, '-o', output_file])
+        return output_file, tmp_file
+    else:
+        tmp_folder = tempfile.mkdtemp()
+        output_zip = os.path.join(tmp_folder, 'output.zip')
+        with zipfile.ZipFile(output_zip, 'w') as zipf:
+            for file in files:
+                tmp_file = os.path.join(tmp_folder, file.filename)
+                file.save(tmp_file)
+                output_file = os.path.join(tmp_folder, f'{file.filename}.pdf')
+                subprocess.run(['mdpdf', tmp_file, '-o', output_file])
+                zipf.write(output_file, f'{file.filename}.pdf')
+        return output_zip, tmp_folder
