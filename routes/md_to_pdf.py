4c74d01b4512256919cc5dc2e1f8c1caab650b81
planetcreative61 creativity <planetcreative61@gmail.com>
2023-11-10 23:26:10 +0000

    added github_md_files_to_pdf

--- routes/md_to_pdf.py
+++ routes/md_to_pdf.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from flask import jsonify, request, send_file, after_this_request
-from tools.markdown_to_pdf import md_text_to_pdf, md_to_pdf
+from tools.markdown_to_pdf import md_text_to_pdf, md_to_pdf, github_md_files_to_pdf
 from utils.utils import validate_file
 import json
 
@@ -24,21 +24,23 @@ def md_text_to_pdf_route(app):
 
 
 
-"""
-    now plesse complete the implementation of the below function to use the md_to_pdf you just created
-    and use the @after_this_request to remove the tmp files
-    if the tmp_files ends with .md then just delete the md file, otherwise remove the directory or somthing
-"""
+
+
 
 def md_to_pdf_route(app):
     @app.route("/api/markdown-to-pdf", methods=['POST'])
     def md_to_pdf_handler():
-        if 'files' not in request.files:
-            return jsonify({"error": "No PDF file provided"}), 400
         files = request.files.getlist('files')
-        
-        pdf_path, tmp_path = md_to_pdf(files)
-        
+        selectedGithubMarkdownUrls = json.loads(request.form.get('selectedGithubMarkdownUrls'))
+
+        if len(files) == 0 and len(selectedGithubMarkdownUrls) == 0:
+            return jsonify({"error": "No Markdown files or GitHub URLs provided"}), 400
+
+        if len(selectedGithubMarkdownUrls) > 0:
+            pdf_path, tmp_path = github_md_files_to_pdf(selectedGithubMarkdownUrls)
+        else:
+            pdf_path, tmp_path = md_to_pdf(files)
+
         @after_this_request
         def remove_tmp_files(response):
             if os.path.isfile(tmp_path) and tmp_path.endswith('.md'):
@@ -46,8 +48,8 @@ def md_to_pdf_route(app):
             else:
                 shutil.rmtree(tmp_path)
             return response
-        
+
         if tmp_path.endswith('.md'):
             return send_file(pdf_path, as_attachment=True)
         else:
-            return send_file(pdf_path, as_attachment=True, attachment_filename='output.zip')
+            return send_file(pdf_path, as_attachment=True, download_name='output.zip')
--- tools/markdown_to_pdf.py
+++ tools/markdown_to_pdf.py
@@ -3,6 +3,8 @@ import subprocess
 import os
 import shutil
 import zipfile
+import requests
+
 
 def md_text_to_pdf(md_text):
     with tempfile.NamedTemporaryFile('w', delete=False, suffix='.md') as temp:
@@ -34,7 +36,45 @@ def md_to_pdf(files):
             for file in files:
                 tmp_file = os.path.join(tmp_folder, file.filename)
                 file.save(tmp_file)
-                output_file = os.path.join(tmp_folder, f'{file.filename}.pdf')
+                filename_without_ext = os.path.splitext(file.filename)[0]
+                output_file = os.path.join(tmp_folder, f'{filename_without_ext}.pdf')
                 subprocess.run(['mdpdf', tmp_file, '-o', output_file])
-                zipf.write(output_file, f'{file.filename}.pdf')
+                zipf.write(output_file, f'{filename_without_ext}.pdf')
+        return output_zip, tmp_folder
+
+
+"""
+    i have a GITHUB_API_KEY env variable that can be used to access the github api
+    please use it to implement this function for me, it's taking a github urls list
+    save each file in the list as a tmp file and convert it to pdfs using the same approach as the md_to_pdf function.
+    or use it if it's possible.
+"""
+
+
+def github_md_files_to_pdf(github_md_files):
+    tmp_folder = tempfile.mkdtemp()
+    if len(github_md_files) == 1:
+        file_info = github_md_files[0]
+        url = file_info['name']
+        response = requests.get(url, headers={'Authorization': f'token {os.getenv("GITHUB_API_KEY")}'})
+        filename_without_ext = os.path.splitext(os.path.basename(url))[0]
+        tmp_file = os.path.join(tmp_folder, f'{filename_without_ext}.md')
+        with open(tmp_file, 'w') as f:
+            f.write(response.text)
+        output_file = os.path.join(tmp_folder, f'{filename_without_ext}.pdf')
+        subprocess.run(f'mdpdf {tmp_file} -o {output_file} > /dev/null 2>&1', shell=True)
+        return output_file, tmp_file
+    else:
+        output_zip = os.path.join(tmp_folder, 'output.zip')
+        with zipfile.ZipFile(output_zip, 'w') as zipf:
+            for file_info in github_md_files:
+                url = file_info['name']
+                response = requests.get(url, headers={'Authorization': f'token {os.getenv("GITHUB_API_KEY")}'})
+                filename_without_ext = os.path.splitext(os.path.basename(url))[0]
+                tmp_file = os.path.join(tmp_folder, f'{filename_without_ext}.md')
+                with open(tmp_file, 'w') as f:
+                    f.write(response.text)
+                output_file = os.path.join(tmp_folder, f'{filename_without_ext}.pdf')
+                subprocess.run(f'mdpdf {tmp_file} -o {output_file} > /dev/null 2>&1', shell=True)
+                zipf.write(output_file, f'{filename_without_ext}.pdf')
         return output_zip, tmp_folder
