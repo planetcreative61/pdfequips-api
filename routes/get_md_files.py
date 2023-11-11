6085e3e8bed6a765d3e1d2672e0938cb1c412e92
planetcreative61 creativity <planetcreative61@gmail.com>
2023-11-11 11:26:38 +0000

    changed github_md_files_urls object architecture

--- routes/get_md_files.py
+++ routes/get_md_files.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 import requests
 import os
 
+
+"""
+    please change the implementation of this function where it also includes the url of the file in the response
+    i.e add a url entity to the markdown_files which is the url of the current md file
+"""
 def get_md_files(app):
     @app.route('/api/get-md-files', methods=['GET'])
     def get_md_files_handler():
@@ -22,12 +27,22 @@ def get_md_files(app):
             data = response.json()
             if isinstance(data, list):  # Check if data is an array, indicating a directory
                 markdown_files = [
-                    {"name": file['name'], "size": file['size']}
+                    {
+                        "name": file['name'],
+                        "size": file['size'],
+                        "url": file['html_url']
+                    }
                     for file in data if file['type'] == 'file' and file['name'].endswith('.md')
                 ]
             else:  # If data is not an array, it's a single file
                 if data['type'] == 'file' and data['name'].endswith('.md'):
-                    markdown_files = [{"name": data['name'], "size": data['size']}]
+                    markdown_files = [
+                        {
+                            "name": data['name'],
+                            "size": data['size'],
+                            "url": data['html_url']
+                        }
+                    ]
                 else:
                     markdown_files = []
             return jsonify(markdown_files), 200
--- tools/markdown_to_pdf.py
+++ tools/markdown_to_pdf.py
@@ -44,37 +44,73 @@ def md_to_pdf(files):
 
 
 """
-    i have a GITHUB_API_KEY env variable that can be used to access the github api
-    please use it to implement this function for me, it's taking a github urls list
+    
+    please give me the implementation of this function, it's taking a github urls list
     save each file in the list as a tmp file and convert it to pdfs using the same approach as the md_to_pdf function.
     or use it if it's possible.
+    keep in mind that the github_md_files is a dictionary i.e an object that is comming from the front end as json:
+    which is in the following format: { name: string; size: number }[];
 """
 
 
+"""
+    in this function before i was using response.content as the file content but it's a json file
+    instead i realized that in the content json there is a richText property which contains the md text, i want to use that.
+    but i got these errors:
+    Traceback (most recent call last):
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask/app.py", line 2548, in __call__
+    return self.wsgi_app(environ, start_response)
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask/app.py", line 2528, in wsgi_app
+    response = self.handle_exception(e)
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask_cors/extension.py", line 176, in wrapped_function
+    return cors_after_request(app.make_response(f(*args, **kwargs)))
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask/app.py", line 2525, in wsgi_app
+    response = self.full_dispatch_request()
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask/app.py", line 1822, in full_dispatch_request
+    rv = self.handle_user_exception(e)
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask_cors/extension.py", line 176, in wrapped_function
+    return cors_after_request(app.make_response(f(*args, **kwargs)))
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask/app.py", line 1820, in full_dispatch_request
+    rv = self.dispatch_request()
+  File "/workspace/pdfequips-api/.venv/lib/python3.10/site-packages/flask/app.py", line 1796, in dispatch_request
+    return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
+  File "/workspace/pdfequips-api/routes/md_to_pdf.py", line 40, in md_to_pdf_handler
+    pdf_path, tmp_path = github_md_files_to_pdf(selectedGithubMarkdownUrls)
+  File "/workspace/pdfequips-api/tools/markdown_to_pdf.py", line 65, in github_md_files_to_pdf
+    f.write(response.content.richText)
+AttributeError: 'bytes' object has no attribute 'richText'
+"""
+
 def github_md_files_to_pdf(github_md_files):
-    tmp_folder = tempfile.mkdtemp()
     if len(github_md_files) == 1:
-        file_info = github_md_files[0]
-        url = file_info['name']
-        response = requests.get(url, headers={'Authorization': f'token {os.getenv("GITHUB_API_KEY")}'})
-        filename_without_ext = os.path.splitext(os.path.basename(url))[0]
-        tmp_file = os.path.join(tmp_folder, f'{filename_without_ext}.md')
-        with open(tmp_file, 'w') as f:
-            f.write(response.text)
-        output_file = os.path.join(tmp_folder, f'{filename_without_ext}.pdf')
-        subprocess.run(f'mdpdf {tmp_file} -o {output_file} > /dev/null 2>&1', shell=True)
-        return output_file, tmp_file
+        file = github_md_files[0]
+        url = file['url']
+        response = requests.get(url)
+        if response.status_code == 200:
+            tmp_folder = tempfile.mkdtemp()
+            tmp_file = os.path.join(tmp_folder, file['name'])
+            content = response.json()['content']['richText']
+            with open(tmp_file, 'w') as f:
+                f.write(content)
+            output_file = os.path.join(tmp_folder, 'output.pdf')
+            subprocess.run(['mdpdf', tmp_file, '-o', output_file])
+            return output_file, tmp_folder
+        else:
+            return None, None
     else:
+        tmp_folder = tempfile.mkdtemp()
         output_zip = os.path.join(tmp_folder, 'output.zip')
         with zipfile.ZipFile(output_zip, 'w') as zipf:
-            for file_info in github_md_files:
-                url = file_info['name']
-                response = requests.get(url, headers={'Authorization': f'token {os.getenv("GITHUB_API_KEY")}'})
-                filename_without_ext = os.path.splitext(os.path.basename(url))[0]
-                tmp_file = os.path.join(tmp_folder, f'{filename_without_ext}.md')
-                with open(tmp_file, 'w') as f:
-                    f.write(response.text)
-                output_file = os.path.join(tmp_folder, f'{filename_without_ext}.pdf')
-                subprocess.run(f'mdpdf {tmp_file} -o {output_file} > /dev/null 2>&1', shell=True)
-                zipf.write(output_file, f'{filename_without_ext}.pdf')
+            for file in github_md_files:
+                url = file['url']
+                response = requests.get(url)
+                if response.status_code == 200:
+                    tmp_file = os.path.join(tmp_folder, file['name'])
+                    content = response.json()['content']['richText']
+                    with open(tmp_file, 'w') as f:
+                        f.write(content)
+                    filename_without_ext = os.path.splitext(file['name'])[0]
+                    output_file = os.path.join(tmp_folder, f'{filename_without_ext}.pdf')
+                    subprocess.run(['mdpdf', tmp_file, '-o', output_file])
+                    zipf.write(output_file, f'{filename_without_ext}.pdf')
         return output_zip, tmp_folder
